96cac3b1ac14613bf3cece5df0ab69d8ea2d53e0
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-28 19:54:57 +0800

    fix login

--- app/controllers/org/board/members_controller.rb
+++ app/controllers/org/board/members_controller.rb
@@ -3,15 +3,15 @@ module Org
     before_action :set_member, only: [:show, :edit, :update]
 
     def login
-      @member = current_account.members.find(params[:id])
+      @member = current_user.members.find(params[:id])
       Current.session.update member_id: @member.id
-      refresh_or_redirect_to({ controller: '/me/home' })
+      refresh_or_redirect_to({ controller: '/admin/home' })
     end
 
     def user_login
       @member = current_user.members.find(params[:id])
       Current.session.update member_id: @member.id
-      refresh_or_redirect_to({ controller: '/me/home' })
+      refresh_or_redirect_to({ controller: '/admin/home' })
     end
 
     def logout
