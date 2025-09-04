5b45619f2ace718a0b78fe16912b02c3987cbf17
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-04 14:46:57 +0800

    clear logic

--- app/controllers/org/controller/admin.rb
+++ app/controllers/org/controller/admin.rb
@@ -6,7 +6,23 @@ module Org
       return if current_organ && current_organ.self_and_ancestor_ids.include?(current_member&.organ_id)
 
       if Current.session
-        render 'require_org_member', layout: 'simple'
+        if current_organ
+          render 'require_org_member', layout: 'simple'
+        else
+          if current_account
+            members = current_account.members.includes(:organ)
+          elsif current_user
+            members = current_user.members.includes(:organ)
+          else
+            members = Member.none
+          end
+
+          if members.blank?
+          else
+
+          end
+
+        end
       elsif defined? current_provider_app
         require_user(current_provider_app)
       else
