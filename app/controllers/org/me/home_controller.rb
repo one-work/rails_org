30100a7d66f7a562ccefe041c6db86bf47020050
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-07 15:15:34 +0800

    skip before action

--- app/controllers/org/me/home_controller.rb
+++ app/controllers/org/me/home_controller.rb
@@ -1,6 +1,6 @@
 module Org
   class Me::HomeController < Me::BaseController
-    skip_before_action :require_role
+    skip_before_action :require_role, only: [:index] if whether_filter :require_role
 
     def index
     end
