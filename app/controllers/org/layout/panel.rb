c7c6386ed5eb18439ca858a104868bc67c84026d
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-02 21:27:12 +0800

    layout

--- app/controllers/org/layout/panel.rb
+++ app/controllers/org/layout/panel.rb
@@ -2,10 +2,6 @@ module Org
   module Layout::Panel
     extend ActiveSupport::Concern
 
-    included do
-      layout 'panel'
-    end
-
     class_methods do
       def local_prefixes
         [controller_path, 'org/panel/base', 'panel']
