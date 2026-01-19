5a3d6065eded72e96378f87c94cdc1b66fec7bd8
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-19 12:44:56 +0800

    moved unsued

--- app/controllers/org/panel/members_controller.rb
+++ app/controllers/org/panel/members_controller.rb
@@ -19,8 +19,10 @@ module Org
     end
 
     def mock
-      response.headers['Access-Control-Allow-Headers'] = 'Timezone'
-      redirect_to({ controller: '/admin/home', host: @organ.admin_host, auth_token: @member.auth_token }, allow_other_host: true)
+      redirect_to(
+        { controller: '/admin/home', host: @organ.admin_host, auth_token: @member.auth_token },
+        allow_other_host: true
+      )
     end
 
     def edit_roles
