236a7141e13283213a72a16ca1393ac05c00e8fe
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-10 22:33:46 +0800

    without uid

--- app/models/org/ext/oauth_user.rb
+++ app/models/org/ext/oauth_user.rb
@@ -3,7 +3,7 @@ module Org
     extend ActiveSupport::Concern
 
     included do
-      has_many :members, class_name: 'Org::Member', primary_key: [:uid, :identity], foreign_key: [:wechat_openid, :identity]
+      has_many :members, class_name: 'Org::Member', primary_key: :identity, foreign_key: :identity
       has_many :organs, class_name: 'Org::Organ', through: :members
     end
 
