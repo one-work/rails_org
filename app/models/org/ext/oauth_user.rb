6bc7a2afbf248548d09c6eb7833efb26932e412a
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-11 23:01:06 +0800

    change tmp members

--- app/models/org/ext/oauth_user.rb
+++ app/models/org/ext/oauth_user.rb
@@ -3,8 +3,15 @@ module Org
     extend ActiveSupport::Concern
 
     included do
+      has_many :temp_members, class_name: 'Org::Member', primary_key: :uid, foreign_key: :identity
       has_many :members, class_name: 'Org::Member', primary_key: :identity, foreign_key: :identity
       has_many :organs, class_name: 'Org::Organ', through: :members
+
+      after_save :sync_to_temp_members, -> { saved_change_to_identity? && identity != uid }
+    end
+
+    def sync_to_temp_members
+      self.temp_members.update_all(identity: identity)
     end
 
   end
