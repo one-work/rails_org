5fe9f0d659723b4f9a195806bcd4c7a551f00fb5
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-09 14:54:33 +0800

    sync temp members

--- app/models/org/ext/oauth_user.rb
+++ app/models/org/ext/oauth_user.rb
@@ -7,7 +7,7 @@ module Org
       has_many :members, class_name: 'Org::Member', primary_key: :identity, foreign_key: :identity
       has_many :organs, class_name: 'Org::Organ', through: :members
 
-      after_save :sync_to_temp_members, -> { saved_change_to_identity? && identity != uid }
+      after_save :sync_to_temp_members, if: -> { saved_change_to_identity? && identity != uid }
     end
 
     def sync_to_temp_members
